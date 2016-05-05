0e71c0c8e55e089bc8e4440267b5536f8f4d0b3e
Edgar Costa <edgarcosta@nyu.edu>
2016-05-04 23:39:37 -0400

    typo fix

--- scripts/mount_buckets.sh
+++ scripts/mount_buckets.sh
@@ -2,7 +2,7 @@
 
 set -e
 #mount buckets
-mkdir -p  /home/lmfdb/buckets/class-groups-quadratic-imaginary-fields/  /home/lmfdb/buckets/class-groups-quadratic-imaginary-fields/ 
+mkdir -p  /home/lmfdb/buckets/riemann-zeta-zeros/  /home/lmfdb/buckets/class-groups-quadratic-imaginary-fields/ 
 gcsfuse --type-cache-ttl "1h" --stat-cache-ttl "1h" riemann-zeta-zeros /home/lmfdb/buckets/riemann-zeta-zeros/
 gcsfuse --type-cache-ttl "1h" --stat-cache-ttl "1h" class-groups-quadratic-imaginary-fields class-groups-quadratic-imaginary-fields/ 
 
