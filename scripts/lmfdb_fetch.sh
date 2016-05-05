7a33dce94221b504469b843aba7875c880d27637
Edgar Costa <edgarcosta@math.dartmouth.edu>
2016-05-05 17:29:03 -0400

    pull->fetch

--- scripts/lmfdb_fetch.sh
+++ scripts/lmfdb_fetch.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 date
-pushd /home/lmfdb/lmfdb.git
-git fetch origin prod
-git fetch origin beta
+pushd /home/lmfdb/lmfdb-git-prod
+git fetch 
+git checkout prod -f 
+popd
+pushd /home/lmfdb/lmfdb-git-beta
+git fetch
+git checkout beta -f
 popd
