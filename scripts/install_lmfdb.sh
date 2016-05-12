80f1907f68b6f3135e9a61b38bad2ed42688f75e
Edgar Costa <edgarcosta@math.dartmouth.edu>
2016-05-12 12:28:35 -0400

    taking into account the new mongoclietconfigs

--- scripts/install_lmfdb.sh
+++ scripts/install_lmfdb.sh
@@ -29,13 +29,24 @@ unset GIT_WORK_TREE
 popd
 
 git clone https://github.com/edgarcosta/lmfdb-gce.git /home/lmfdb/lmfdb-gce 
+mkdir -p /home/lmfdb/logs/beta /home/lmfdb/logs/prod 
+git clone https://github.com/LMFDB/lmfdb.git /home/lmfdb/lmfdb-git-beta
+git clone https://github.com/LMFDB/lmfdb.git /home/lmfdb/lmfdb-git-prod
 
-#take care of the hook
-ln -s /home/lmfdb/lmfdb-gce/scripts/post-receive /home/lmfdb/lmfdb.git/hooks/post-receive 
+#checkout beta
+pushd /home/lmfdb/lmfdb-git-beta
+git checkout origin/beta -f
+popd
+#checkout prod
+pushd /home/lmfdb/lmfdb-git-prod
+git checkout origin/prod -f
+popd
 
-#add crontab 
+
+ln -s /home/lmfdb/lmfdb-gce/scripts/post-checkout-beta /home/lmfdb/lmfdb-git-beta/.git/hooks/post-checkout 
+ln -s /home/lmfdb/lmfdb-gce/scripts/post-checkout-prod /home/lmfdb/lmfdb-git-prod/.git/hooks/post-checkout 
 crontab  /home/lmfdb/lmfdb-gce/scripts/crontab
-
+#linking (re)start and stop scripts
 #linking (re)start and stop scripts
 ln -s /home/lmfdb/lmfdb-gce/scripts/start-beta /home/lmfdb/start-beta 
 ln -s /home/lmfdb/lmfdb-gce/scripts/start-prod /home/lmfdb/start-prod 
@@ -45,8 +56,8 @@ ln -s /home/lmfdb/lmfdb-gce/scripts/restart-prod /home/lmfdb/restart-prod
 #linking config files
 ln -s /home/lmfdb/lmfdb-gce/config/gunicorn-config-beta /home/lmfdb/lmfdb-git-beta 
 ln -s /home/lmfdb/lmfdb-gce/config/gunicorn-config-prod /home/lmfdb/lmfdb-git-prod 
-ln -s /home/lmfdb/lmfdb-gce/config/mongoclient.config  /home/lmfdb/lmfdb-git-beta 
-ln -s /home/lmfdb/lmfdb-gce/config/mongoclient.config  /home/lmfdb/lmfdb-git-prod 
+ln -s /home/lmfdb/lmfdb-gce/config/mongoclient_lmfdb0.config  /home/lmfdb/lmfdb-git-beta/mongoclient.config 
+ln -s /home/lmfdb/lmfdb-gce/config/mongoclient_lmfdb0.config  /home/lmfdb/lmfdb-git-prod/mongoclient.config
 
 # Read Password
 echo -n MongoDB Password: 
