0d6ea9af866c734c0d62b1293009585710a94a20
Edgar Costa <edgarcosta@math.dartmouth.edu>
2016-05-12 12:29:35 -0400

    switch between mongodb servers

---
+++ scripts/read_from_ms.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+_user="$(id -u -n)"
+_uid="$(id -u)"
+echo "User name : $_user"
+echo "User name ID (UID) : $_uid"
+#uid = 1200
+if [ $_uid -ne 1300 ]
+then
+    echo "Only the user sage with UID = 1300 should be running this script"; exit 1;
+fi
+
+
+rm /home/lmfdb/lmfdb-git-beta/mongoclient.config
+rm /home/lmfdb/lmfdb-git-beta/mongoclient.config
+ln -s /home/lmfdb/lmfdb-gce/config/mongoclient_ms.config  /home/lmfdb/lmfdb-git-beta/mongoclient.config 
+ln -s /home/lmfdb/lmfdb-gce/config/mongoclient_ms.config  /home/lmfdb/lmfdb-git-prod/mongoclient.config
+
+/home/lmfdb/lmfdb-gce/scripts/restart-prod
+/home/lmfdb/lmfdb-gce/scripts/restart-beta
