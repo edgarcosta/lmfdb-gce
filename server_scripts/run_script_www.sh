4401100046e6c73f43b02b3c96dcdb5392a8695c
Edgar Costa <edgarcosta@nyu.edu>
2016-05-14 22:37:04 -0400

    a new example

--- server_scripts/run_script_www.sh
+++ server_scripts/run_script_www.sh
@@ -1,7 +1,8 @@
 echo "Will be running $1 on www-central*"
 #eg bash run_script_www.sh "cd lmfdb-gce/ && git pull && git status"
+#eg bash run_script_www.sh "cd lmfdb-gce/ && git pull && git status && bash server_scripts/update_gce.sh"
 
-for i in {0..4};
+for i in {0..3};
 do echo  www-central$i.lmfdb.xyz;
 ssh www-central$i.lmfdb.xyz $1;
 done
