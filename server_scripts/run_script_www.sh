982e53dfee31e47e55843d526c357d5e2eea7487
Edgar Costa <edgarcosta@nyu.edu>
2016-05-14 23:32:08 -0400

    touching the startup scripts

--- server_scripts/run_script_www.sh
+++ server_scripts/run_script_www.sh
@@ -1,6 +1,8 @@
 echo "Will be running $1 on www-central*"
-#eg bash run_script_www.sh "cd lmfdb-gce/ && git pull && git status"
-#eg bash run_script_www.sh "cd lmfdb-gce/ && git pull && git status && bash server_scripts/update_gce.sh"
+#eg:
+#bash run_script_www.sh "cd lmfdb-gce/ && git pull && git status"
+#bash run_script_www.sh "cd lmfdb-gce/ && git pull && git status && bash server_scripts/update_gce.sh"
+#bash run_script_www.sh "sudo su lmfdb -c \"bash /home/lmfdb/lmfdb-gce/scripts/lmfdb_fetch.sh\""
 
 for i in {0..3};
 do echo  www-central$i.lmfdb.xyz;
--- server_scripts/startup_script.sh
+++ server_scripts/startup_script.sh
@@ -5,5 +5,8 @@ su lmfdb -c "mkdir -p  /home/lmfdb/data /home/lmfdb/buckets/riemann-zeta-zeros/ 
 mount /home/lmfdb/data 
 su lmfdb -c "gcsfuse riemann-zeta-zeros /home/lmfdb/buckets/riemann-zeta-zeros/" 
 su lmfdb -c "gcsfuse class-groups-quadratic-imaginary-fields /home/lmfdb/buckets/class-groups-quadratic-imaginary-fields/"
+#updating gits files
+su lmfdb -c "cd /home/lmfdb/lmfdb-gce/ && git fetch && git checkout -f origin/master"
+su lmfdb -c "bash /home/lmfdb/lmfdb-gce/scripts/lmfdb_fetch.sh"
 su lmfdb -c "bash /home/lmfdb/start-prod"
 set +e
