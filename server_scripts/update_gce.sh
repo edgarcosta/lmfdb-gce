150d931c1f434893d16dde1b823a8f0d4b1800c6
Edgar Costa <edgarcosta@nyu.edu>
2016-05-14 22:25:58 -0400

    sudo -u eof

--- server_scripts/update_gce.sh
+++ server_scripts/update_gce.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 set -e
-sudo su lmfdb
+sudo -u lmfdb bash  - eof<<
+set -e
 cd ~/lmfdb-gce
 git status
 echo "waiting 5s"
 sleep 5s;
 git fetch
 git checkout -f origin/master
-logout
+set +e
+eof
+
 set +e
