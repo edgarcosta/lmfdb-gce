f828587d30e0ee17d0ecb19489dd850b148b4da6
Edgar Costa <edgarcosta@math.dartmouth.edu>
2016-05-16 15:19:53 -0400

    more examples

--- server_scripts/run_script_www.sh
+++ server_scripts/run_script_www.sh
@@ -3,8 +3,12 @@ echo "Will be running $1 on www-central*"
 #bash run_script_www.sh "cd lmfdb-gce/ && git pull && git status"
 #bash run_script_www.sh "cd lmfdb-gce/ && git pull && git status && bash server_scripts/update_gce.sh"
 #bash run_script_www.sh "sudo su lmfdb -c \"bash /home/lmfdb/lmfdb-gce/scripts/lmfdb_fetch.sh\""
+#bash run_script_www.sh "sudo su lmfdb -c \"bash /home/lmfdb/lmfdb-gce/scripts/read_from_lmfdb0.sh\""
+#bash run_script_www.sh "sudo su lmfdb -c \"bash /home/lmfdb/lmfdb-gce/scripts/read_from_ms.sh\""
 
-for i in {0..3};
+
+
+for i in {0..1};
 do echo  www-central$i.lmfdb.xyz;
 ssh www-central$i.lmfdb.xyz $1;
 done
