88d20bd766f38ddbc412c9f9b7dbda7c84458461
Edgar Costa <edgarcosta@math.dartmouth.edu>
2016-05-19 16:48:42 -0400

    db_scripts

---
+++ db_scripts/update-classical-mf.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo "do we need to worry about the jsons??????"
+echo "sleeping 5s"
+sleep 5s
+set -e;
+for coll in "dimension_table" "dimension_table.chunks" "dimension_table.files" "webmodformspace" "webmodformspace.chunks" "webmodformspace.files" "webnewforms" "webnewforms.chunks" "webnewforms.files" "webeigenvalues.chunks" "webeigenvalues.files"
+do
+mongodump --host m0 -u $READ_USER -p $READ_PASS --authenticationDatabase admin -o /mnt/tmp/scratch --db modularforms2 --collection ${coll}
+newcoll=new_${coll}
+mongorestore -u $WRITE_USER -p $WRITE_PASS --authenticationDatabase admin --db modularforms2 --collection ${newcoll} /mnt/tmp/scratch/modularforms2/${coll}.bson
+done;
+for coll in "dimension_table" "dimension_table.chunks" "dimension_table.files" "webmodformspace" "webmodformspace.chunks" "webmodformspace.files" "webnewforms" "webnewforms.chunks" "webnewforms.files" "webeigenvalues.chunks" "webeigenvalues.files"
+do
+mongo modularforms2 -u $WRITE_USER -p $WRITE_PASS --authenticationDatabase admin --eval "printjson(db.${coll}.renameCollection('old_${coll}'))"
+mongo modularforms2 -u $WRITE_USER -p $WRITE_PASS --authenticationDatabase admin --eval "printjson(db.new_${coll}.renameCollection('${coll}'))"
+done;
+rm -rf /mnt/tmp/scratch/modularforms2
+echo "After you verify that everything looks good, run clean-classical-mfd.sh to drop the old collections"
+set +e;
