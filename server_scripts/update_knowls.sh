79906c4ee55357121353381358b17d0db19d074b
Edgar Costa <edgarcosta@math.dartmouth.edu>
2016-05-19 14:13:45 -0400

    m1 -> m0

--- server_scripts/update_knowls.sh
+++ server_scripts/update_knowls.sh
@@ -5,7 +5,7 @@ set -e
 pushd ~/dump/
 timestamp=`date -u +%Y%m%d-%H%M`
 mkdir $timestamp
-mongodump --host m1 -u $MONGO_USERNAME -p $MONGO_PASSWORD --authenticationDatabase admin -o $timestamp --db knowledge
+mongodump --host m0 -u $MONGO_USERNAME -p $MONGO_PASSWORD --authenticationDatabase admin -o $timestamp --db knowledge
 rm -rf $timestamp/knowledge/system.profile*
 rm -rf $timestamp/knowledge/system.users*
 time mongorestore  -u $MONGO_USERNAME -p $MONGO_PASSWORD --authenticationDatabase admin  --db knowledge --drop $timestamp/knowledge
