f86627f44de59c04ab7b9efb8d8fdfb3e578a47b
edgarcosta <edgarcosta@math.dartmouth.edu>
2016-05-20 23:53:30 -0400

    Update update-classical-mf.sh

--- db_scripts/update-classical-mf.sh
+++ db_scripts/update-classical-mf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
 echo "do we need to worry about the jsons??????"
+echo "Don't forget to set and export READ_USER, READ_PASS, WRITE_USER, WRITE_PASS"
 echo "sleeping 5s"
 sleep 5s
 set -e;
@@ -8,7 +8,7 @@ for coll in "dimension_table" "dimension_table.chunks" "dimension_table.files" "
 do
 mongodump --host m0 -u $READ_USER -p $READ_PASS --authenticationDatabase admin -o /mnt/tmp/scratch --db modularforms2 --collection ${coll}
 newcoll=new_${coll}
-mongorestore -u $WRITE_USER -p $WRITE_PASS --authenticationDatabase admin --db modularforms2 --collection ${newcoll} /mnt/tmp/scratch/modularforms2/${coll}.bson
+mongorestore -u $WRITE_USER -p $WRITE_PASS --authenticationDatabase modularforms2 --db modularforms2 --collection ${newcoll} /mnt/tmp/scratch/modularforms2/${coll}.bson
 done;
 for coll in "dimension_table" "dimension_table.chunks" "dimension_table.files" "webmodformspace" "webmodformspace.chunks" "webmodformspace.files" "webnewforms" "webnewforms.chunks" "webnewforms.files" "webeigenvalues.chunks" "webeigenvalues.files"
 do
