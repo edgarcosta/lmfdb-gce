092a37f48b1b575520bf0febe4fd1d14f16de699
Šarūnas Burdulis <sarunas@mail.saabnet.com>
2016-05-27 17:29:33 +0000

    Gunicorn worker benchmarks.

---
+++ ab/wwwcentral2_n1highcpu8_gunicorn/run_ab2a.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+N=10000
+C=5
+base_url='http://www-central2.lmfdb.xyz'
+lmfdb_urls=(
+'/L/degree1/'
+'/L/degree2/'
+'/L/degree3/'
+'/L/degree4/'
+)
+
+for u in ${lmfdb_urls[@]}; do
+    url=${base_url}${u}
+    ufname="wt_gevent_ka_${u//\//_}${C}_${N}"
+    xterm -T "${u}" -e "ab -r -k -g ${ufname}.tsv -c$C -n$N ${url};read" &
+done
+
+#> Pages that hist the database lightly/moderately would be:
+#>
+#> http://www.lmfdb.org/zeros/zeta/
+#> http://www.lmfdb.org/ModularForm/GL2/Q/holomorphic/13/8/1/b/
+#> http://www.lmfdb.org/L/ModularForm/GL2/Q/holomorphic/5/6/4/a/0/
+#> http://www.lmfdb.org/NumberField/6.0.9747.1
+#> http://www.lmfdb.org/EllipticCurve/Q/11/a/2
+#> http://www.lmfdb.org/Genus2Curve/Q/1369/a/50653/1
+#>
+#> Here are some pages that hit the database pretty hard:
+#>
+#> http://www.lmfdb.org/EllipticCurve/browse/
+#> http://www.lmfdb.org/EllipticCurve/Q/stats
+#> http://www.lmfdb.org/NumberField/?ram_primes=11
+#> http://www.lmfdb.org/Genus2Curve/Q/stats
+#> http://beta.lmfdb.org/ModularForm/GL2/Q/holomorphic/23/38/1/?group=0
+#> http://beta.lmfdb.org/ModularForm/GL2/Q/holomorphic/23/38/1/a/
+#> http://beta.lmfdb.org/ModularForm/GL2/Q/holomorphic/23/40/1/?group=0
+#> http://beta.lmfdb.org/ModularForm/GL2/Q/holomorphic/23/40/1/a/
