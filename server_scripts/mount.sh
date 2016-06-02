a3ec33b2f68bb77a6bf915cf53dac9f2f92b9912
Edgar Costa <edgarcosta@math.dartmouth.edu>
2016-06-02 12:32:09 -0400

    trying out different bucket types

--- server_scripts/mount.sh
+++ server_scripts/mount.sh
@@ -3,6 +3,6 @@ set -e
 echo "Mounting the data disk and the buckets"
 sudo su lmfdb -c "mkdir -p  /home/lmfdb/data /home/lmfdb/buckets/riemann-zeta-zeros/  /home/lmfdb/buckets/class-groups-quadratic-imaginary-fields/" 
 sudo mount /home/lmfdb/data 
-sudo su lmfdb -c "gcsfuse riemann-zeta-zeros /home/lmfdb/buckets/riemann-zeta-zeros/" 
-sudo su lmfdb -c "gcsfuse class-groups-quadratic-imaginary-fields /home/lmfdb/buckets/class-groups-quadratic-imaginary-fields/" 
+sudo su lmfdb -c "gcsfuse --type-cache-ttl "1h" --stat-cache-ttl "1h" riemann-zeta-zeros-dra /home/lmfdb/buckets/riemann-zeta-zeros/" 
+sudo su lmfdb -c "gcsfuse --type-cache-ttl "1h" --stat-cache-ttl "1h" class-groups-quadratic-imaginary-fields-nearline /home/lmfdb/buckets/class-groups-quadratic-imaginary-fields/" 
 set +e
--- server_scripts/startup_script.sh
+++ server_scripts/startup_script.sh
@@ -3,8 +3,8 @@ set -e
 echo "Mounting the data disk and the buckets"
 su lmfdb -c "mkdir -p  /home/lmfdb/data /home/lmfdb/buckets/riemann-zeta-zeros/  /home/lmfdb/buckets/class-groups-quadratic-imaginary-fields/" 
 mount /home/lmfdb/data 
-su lmfdb -c "gcsfuse riemann-zeta-zeros /home/lmfdb/buckets/riemann-zeta-zeros/" 
-su lmfdb -c "gcsfuse class-groups-quadratic-imaginary-fields /home/lmfdb/buckets/class-groups-quadratic-imaginary-fields/"
+su lmfdb -c "gcsfuse --type-cache-ttl "1h" --stat-cache-ttl "1h" riemann-zeta-zeros-dra /home/lmfdb/buckets/riemann-zeta-zeros/" 
+su lmfdb -c "gcsfuse --type-cache-ttl "1h" --stat-cache-ttl "1h" class-groups-quadratic-imaginary-fields-nearline /home/lmfdb/buckets/class-groups-quadratic-imaginary-fields/"
 #updating gits files
 su lmfdb -c "cd /home/lmfdb/lmfdb-gce/ && git fetch && git checkout -f origin/master"
 su lmfdb -c "bash /home/lmfdb/lmfdb-gce/scripts/lmfdb_fetch.sh"
