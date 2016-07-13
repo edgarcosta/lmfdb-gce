f13cb3ac52382ccbd8a3accb7142e3ab48d5858f
Edgar Costa <edgarcosta@nyu.edu>
2016-07-13 14:44:50 +1000

    type change

--- db_scripts/update_knowls.sh
+++ db_scripts/update_knowls.sh
@@ -1,1 +1,14 @@
-../server_scripts/update_knowls.sh
+#!/usr/bin/env bash
+
+set -e
+. ~/userpassword
+pushd ~/dump/
+timestamp=`date -u +%Y%m%d-%H%M`
+mkdir $timestamp
+mongodump --host m0 -u $MONGO_USERNAME -p $MONGO_PASSWORD --authenticationDatabase admin -o $timestamp --db knowledge
+rm -rf $timestamp/knowledge/system.profile*
+rm -rf $timestamp/knowledge/system.users*
+time mongorestore  -u $MONGO_USERNAME -p $MONGO_PASSWORD --authenticationDatabase admin  --db knowledge --drop $timestamp/knowledge
+rm -rf $timestamp
+popd
+set +e
