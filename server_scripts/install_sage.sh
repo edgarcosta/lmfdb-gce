c311aff53467014bffcc72a77d6964cfbf3a00e4
edgarcosta <edgarcosta@math.dartmouth.edu>
2016-08-05 09:56:24 -0400

    Update install_sage.sh

--- server_scripts/install_sage.sh
+++ server_scripts/install_sage.sh
@@ -3,9 +3,9 @@ _user="$(id -u -n)"
 _uid="$(id -u)"
 echo "User name : $_user"
 echo "User name ID (UID) : $_uid"
-j=4;
+j=1;
 echo "Using $j threads to compile"
-
+set -e
 #uid = 1200
 if [ $_uid -ne 1200 ]
 then
@@ -22,26 +22,25 @@ sleep 5s;
 
 version=$1
 #sudo apt-get install binutils gcc g++ gfortran make m4 perl tar git libssl-dev
-wget http://mirrors.xmission.com/sage/src/sage-${version}.tar.gz &&
-tar xf sage-${version}.tar.gz && 
-cd sage-${version} &&
+wget http://mirrors.xmission.com/sage/src/sage-${version}.tar.gz 
+tar xf sage-${version}.tar.gz &
+cd sage-${version} 
 #it can get stuck here while building documentation
-MAKE="make -j4" make &&
-make ptest &&
-make ptestlong &&
-./sage -i gap_packages &&
-./sage -i database_gap &&
-./sage -i pip &&
-./sage -b &&
-wget https://raw.githubusercontent.com/LMFDB/lmfdb/master/requirements.txt &&
-./sage -pip install -r requirements.txt &&
-./sage -pip install gunicorn &&
-./sage -pip install greenlet eventlet gevent &&
-./sage -b &&
-cd .. &&
-chmod a+rX -R sage-${version} &&
-echo "If you want this to be the new version to be used, don't forget to do:" &&
-echo "$ rm sage-root && ln -s sage-${version}" &&
-exit 0;
-
+MAKE="make -j${j}" make 
+make ptest 
+make ptestlong 
+./sage -i gap_packages 
+./sage -i database_gap 
+./sage -i pip 
+./sage -b 
+wget https://raw.githubusercontent.com/LMFDB/lmfdb/master/requirements.txt 
+./sage -pip install -r requirements.txt 
+./sage -pip install gunicorn pyflakes
+./sage -pip install greenlet eventlet gevent 
+./sage -b 
+cd .. 
+chmod a+rX -R sage-${version} 
+echo "If you want this to be the new version to be used, don't forget to do:" 
+echo "$ rm sage-root && ln -s sage-${version}" 
+set +e
 
