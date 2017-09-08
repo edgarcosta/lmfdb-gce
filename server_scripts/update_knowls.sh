287ac3dde001b87de0c45196b4421b6525e8b035
Edgar Costa <edgarcosta@math.dartmouth.edu>
2017-09-08 11:02:12 -0400

    parsing command before running it

--- server_scripts/update_knowls.sh
+++ server_scripts/update_knowls.sh
@@ -5,10 +5,10 @@ set -e
 pushd ~/dump/
 timestamp=`date -u +%Y%m%d-%H%M`
 mkdir $timestamp
-mongodump --host m0 -u $MONGO_USERNAME -p $MONGO_PASSWORD --authenticationDatabase admin -o $timestamp --db knowledge
+eval mongodump --host m0 -u ${MONGO_USERNAME} -p ${MONGO_PASSWORD} --authenticationDatabase admin -o $timestamp --db knowledge
 rm -rf $timestamp/knowledge/system.profile*
 rm -rf $timestamp/knowledge/system.users*
-time mongorestore  -u $MONGO_USERNAME -p $MONGO_PASSWORD --authenticationDatabase admin  --db knowledge --drop $timestamp/knowledge
+eval time mongorestore  -u ${MONGO_USERNAME} -p ${MONGO_PASSWORD} --authenticationDatabase admin  --db knowledge --drop $timestamp/knowledge
 rm -rf $timestamp
 popd
 set +e
