4a4447e14e6a80935d5dbfa5ebeb7fb46c2c6dd1
John Cremona <john.cremona@gmail.com>
2018-01-26 14:58:58 +0000

    added scripts for updating inventory

---
+++ server_scripts/update_inventory.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+. ~/userpassword
+pushd ~/dump/
+timestamp=`date -u +%Y%m%d-%H%M`
+mkdir $timestamp
+mongodump --host m0 -u "${MONGO_USERNAME}" -p "${MONGO_PASSWORD}" --authenticationDatabase admin -o "$timestamp" --db inventory
+rm -rf $timestamp/inventory/system.profile*
+rm -rf $timestamp/inventory/system.users*
+time mongorestore  -u "${MONGO_USERNAME}" -p "${MONGO_PASSWORD}" --authenticationDatabase admin  --db knowledge --drop "$timestamp/inventory"
+rm -rf $timestamp
+popd
+set +e
