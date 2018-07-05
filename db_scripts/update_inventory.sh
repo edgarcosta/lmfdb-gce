7b15592860462c836602df54598d6fc891f608eb
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-07-05 18:02:54 -0400

    typechange

--- db_scripts/update_inventory.sh
+++ db_scripts/update_inventory.sh
@@ -1,1 +1,14 @@
-../server_scripts/update_inventory.sh
+#!/usr/bin/env bash
+
+set -e
+. ~/userpassword
+pushd ~/dump/
+timestamp=inventory`date -u +%Y%m%d-%H%M`
+mkdir $timestamp
+mongodump --host m0 -u "${MONGO_USERNAME}" -p "${MONGO_PASSWORD}" --authenticationDatabase admin -o "$timestamp" --db inventory
+rm -rf $timestamp/inventory/system.profile*
+rm -rf $timestamp/inventory/system.users*
+time mongorestore  -u "${MONGO_USERNAME}" -p "${MONGO_PASSWORD}" --authenticationDatabase admin  --db inventory --drop "$timestamp/inventory"
+rm -rf $timestamp
+popd
+set +e
