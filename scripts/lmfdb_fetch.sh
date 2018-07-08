16e03963c655a2654bab2fe6cf9a97abdad68f5a
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-07-08 15:23:17 -0400

    adding fetch for postgres

--- scripts/lmfdb_fetch.sh
+++ scripts/lmfdb_fetch.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 date
 pushd /home/lmfdb/lmfdb-git-prod
-git fetch 
-git checkout origin/prod -f 
+git fetch
+git checkout origin/prod -f
 popd
 pushd /home/lmfdb/lmfdb-git-beta
 git fetch
 git checkout origin/beta -f
 popd
+if [ -d "/home/lmfdb/lmfdb-git-postgres" ]; then
+pushd /home/lmfdb/lmfdb-git-postgres
+git fetch roed314
+git checkout roed314/postgres -f
+popd
+fi
