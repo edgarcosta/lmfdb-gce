74d674d1b928a096a76d2c24a556438e162eaa8c
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-08-06 20:48:51 -0400

    new script to update knowls

---
+++ server_scripts/update_knowls_and_userdb.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+set -e
+if [ $USER != postgres ]
+then
+  echo "must be run as the postgres user"
+  false
+else
+  userdbdump=/scratch/userdb`date -u +%Y%m%d-%H%M`.tar
+  knowlsdump=/scratch/knowls`date -u +%Y%m%d-%H%M`.tar
+  time pg_dump --host devmirror.lmfdb.xyz --clean --if-exists --schema=userdb  -v --file $userdbdump --format tar lmfdb
+  time pg_dump --host devmirror.lmfdb.xyz --clean --if-exists --schema=public -t 'kwl_*'  -v --file $knowlsdump --format tar lmfdb
+  time pg_restore --clean --if-exists --dbname lmfdb $userdbdump
+  time pg_restore --clean --if-exists --dbname lmfdb $knowlsdump
+  rm -rf $userdb $knowls
+  psql --dbname lmfdb --command "REVOKE INSERT, UPDATE, DELETE on kwl_deleted, kwl_history, kwl_knowls FROM webserver;"
+  psql --dbname lmfdb --command "REVOKE SELECT, UPDATE, INSERT, DELETE  ON ALL TABLES IN SCHEMA userdb  FROM webserver;"
+fi
+set +e
