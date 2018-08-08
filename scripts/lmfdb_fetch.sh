501fe3591db326362067964b6dece4cadf5e2729
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-08-08 15:13:53 -0400

    a more sane fetch

--- scripts/lmfdb_fetch.sh
+++ scripts/lmfdb_fetch.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/env bash
 date
+if [ -d "/home/lmfdb/lmfdb-git-prod" ]; then
 pushd /home/lmfdb/lmfdb-git-prod
 git fetch
 git checkout origin/prod -f
 popd
+if [ -d "/home/lmfdb/lmfdb-git-beta" ]; then
 pushd /home/lmfdb/lmfdb-git-beta
 git fetch
 git checkout origin/beta -f
 popd
+fi
 if [ -d "/home/lmfdb/lmfdb-git-postgres" ]; then
 pushd /home/lmfdb/lmfdb-git-postgres
 git fetch roed314 postgres
 git checkout roed314/postgres -f
+popd
+fi
+if [ -d "/home/lmfdb/lmfdb-git-dev" ]; then
+pushd /home/lmfdb/lmfdb-git-dev
+git fetch roed314 postgres
+git checkout roed314/postgres -f
+#git fetch
+#git checkout origin/dev -f
+popd
+fi
+if [ -d "/home/lmfdb/lmfdb-git-web" ]; then
+pushd /home/lmfdb/lmfdb-git-web
+git fetch roed314 postgres
+git checkout roed314/postgres -f
+#git fetch
+#git checkout origin/web -f
 popd
 fi
