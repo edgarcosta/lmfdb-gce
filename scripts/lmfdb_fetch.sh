74a9136e12bbe2270435020c10a8ab124d9cadac
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-08-08 18:06:23 -0400

    missing fi

--- scripts/lmfdb_fetch.sh
+++ scripts/lmfdb_fetch.sh
@@ -5,18 +5,22 @@ pushd /home/lmfdb/lmfdb-git-prod
 git fetch
 git checkout origin/prod -f
 popd
+fi
+
 if [ -d "/home/lmfdb/lmfdb-git-beta" ]; then
 pushd /home/lmfdb/lmfdb-git-beta
 git fetch
 git checkout origin/beta -f
 popd
 fi
+
 if [ -d "/home/lmfdb/lmfdb-git-postgres" ]; then
 pushd /home/lmfdb/lmfdb-git-postgres
 git fetch roed314 postgres
 git checkout roed314/postgres -f
 popd
 fi
+
 if [ -d "/home/lmfdb/lmfdb-git-dev" ]; then
 pushd /home/lmfdb/lmfdb-git-dev
 git fetch roed314 postgres
@@ -25,6 +29,7 @@ git checkout roed314/postgres -f
 #git checkout origin/dev -f
 popd
 fi
+
 if [ -d "/home/lmfdb/lmfdb-git-web" ]; then
 pushd /home/lmfdb/lmfdb-git-web
 git fetch roed314 postgres
@@ -33,3 +38,4 @@ git checkout roed314/postgres -f
 #git checkout origin/web -f
 popd
 fi
+
