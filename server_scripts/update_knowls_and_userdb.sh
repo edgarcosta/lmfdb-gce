d6f617c4ff61a91057c5065ffe10ae24178b97d1
Edgar Costa <edgarcosta@math.dartmouth.edu>
2019-01-20 20:12:55 -0500

    just users

--- server_scripts/update_knowls_and_userdb.sh
+++ server_scripts/update_knowls_and_userdb.sh
@@ -12,13 +12,13 @@ else
   echo timestamp = `date -u +%Y%m%d-%H%M`
   userdbdump=/scratch/knowls_userdb/userdb`date -u +%Y%m%d-%H%M`.tar
   knowlsdump=/scratch/knowls_userdb/knowls`date -u +%Y%m%d-%H%M`.tar
-  time pg_dump --host devmirror --clean --if-exists --schema=userdb  -v --file $userdbdump --format tar lmfdb
+  time pg_dump --host devmirror --clean --if-exists --schema=userdb -t 'users' -v --file $userdbdump --format tar lmfdb
   time pg_dump --host devmirror --clean --if-exists --schema=public -t 'kwl_*'  -v --file $knowlsdump --format tar lmfdb
   time pg_restore --clean --if-exists --dbname lmfdb $userdbdump
   time pg_restore --clean --if-exists --dbname lmfdb $knowlsdump
   rm -rf $userdbdump $knowlsdump
-  psql --dbname lmfdb --command "REVOKE INSERT, UPDATE, DELETE on kwl_deleted, kwl_history, kwl_knowls FROM webserver;"
-  psql --dbname lmfdb --command "REVOKE UPDATE, INSERT, DELETE  ON ALL TABLES IN SCHEMA userdb  FROM webserver;"
+  psql --dbname lmfdb --command "REVOKE INSERT, UPDATE, DELETE ON kwl_deleted, kwl_history, kwl_knowls FROM webserver;"
+  psql --dbname lmfdb --command "REVOKE INSERT, UPDATE, DELETE  ON ALL TABLES IN SCHEMA userdb  FROM webserver;"
   echo done
 fi
 set +e
