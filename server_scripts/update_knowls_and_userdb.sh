641d8a6991d3147cbb7265146199be2fa3060440
Edgar Costa <edgarc@mit.edu>
2019-03-13 18:23:45 -0400

    new scripts for knowls

--- server_scripts/update_knowls_and_userdb.sh
+++ server_scripts/update_knowls_and_userdb.sh
@@ -13,12 +13,12 @@ else
   userdbdump=/scratch/knowls_userdb/userdb`date -u +%Y%m%d-%H%M`.tar
   knowlsdump=/scratch/knowls_userdb/knowls`date -u +%Y%m%d-%H%M`.tar
   time pg_dump --host devmirror --clean --if-exists --schema=userdb -t 'userdb.users' -v --file $userdbdump --format tar lmfdb
-  time pg_dump --host devmirror --clean --if-exists --schema=public -t 'kwl_*'  -v --file $knowlsdump --format tar lmfdb
+  time pg_dump --host devmirror --clean --if-exists --schema=public -t 'kwl_(knowls|locks)'  -v --file $knowlsdump --format tar lmfdb
   time pg_restore --clean --if-exists --dbname lmfdb $userdbdump
   time pg_restore --clean --if-exists --dbname lmfdb $knowlsdump
   du -sh $userdbdump $knowlsdump
   rm -rf $userdbdump $knowlsdump
-  psql --dbname lmfdb --command "REVOKE INSERT, UPDATE, DELETE ON kwl_deleted, kwl_history, kwl_knowls FROM webserver;"
+  psql --dbname lmfdb --command "REVOKE INSERT, UPDATE, DELETE ON kwl_locks, kwl_knowls FROM webserver;"
   psql --dbname lmfdb --command "REVOKE INSERT, UPDATE, DELETE  ON ALL TABLES IN SCHEMA userdb  FROM webserver;"
   echo done
 fi
