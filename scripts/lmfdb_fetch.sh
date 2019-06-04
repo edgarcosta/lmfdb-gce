078fdb53c98b9f524886bf3e0d50bff87a42f7e2
Edgar Costa <edgarc@mit.edu>
2019-06-04 09:25:40 -0400

    redoing fetch

--- scripts/lmfdb_fetch.sh
+++ scripts/lmfdb_fetch.sh
@@ -4,110 +4,47 @@ date
 # web.lmfdb.xyz=prodweb?.lmfdb.xyz and legendre.mit.edu
 # gunicorn-config-web
 # running on 8080
-if [ -d "/home/lmfdb/lmfdb-git-web" ]; then
-pushd /home/lmfdb/lmfdb-git-web
-git fetch
-git checkout origin/web -f
-popd
-fi
-
-# beta.lmfdb.org=beta.lmfdb.xyz=dev.lmfdb.xyz
-# gunicorn-config-dev
-# running on 8081
-if [ -d "/home/lmfdb/lmfdb-git-dev" ]; then
-pushd /home/lmfdb/lmfdb-git-dev
-git fetch
-git checkout origin/dev -f
-popd
-fi
-
-
-# cmfs.lmfdb.xyz=scratch.lmfdb.xyz
-# gunicorn-config-master
-# running on 8083
-if [ -d "/home/lmfdb/lmfdb-git-master" ]; then
-pushd /home/lmfdb/lmfdb-git-master
-git fetch roed314 master
-git checkout roed314/master -f
-popd
-fi
-
-# abvar.lmfdb.xyz
-# gunicorn-config-abvar
-# running on 8084
-if [ -d "/home/lmfdb/lmfdb-git-abvar" ]; then
-pushd /home/lmfdb/lmfdb-git-abvar
-git fetch roed314 abvar
-git checkout roed314/abvar -f
-popd
-fi
-
-
+#
+# beta.lmfdb.org=beta.lmfdb.xyz=dev.lmfdb.xyz running on 8081 gunicorn-config-dev
 # MONGO
-# www.lmfdb.org www-central?.lmfdb.xyz
-# gunicorn-config-prod
-# running on 8080
-if [ -d "/home/lmfdb/lmfdb-git-prod" ]; then
-pushd /home/lmfdb/lmfdb-git-prod
-git fetch
-git checkout origin/prod -f
-popd
-fi
+# prod.lmfdb.xyz running on 8080 gunicorn-config-prod
+
+for branch in web dev prod do
+  if [ -d "/home/lmfdb/lmfdb-git-$branch" ]; then
+    pushd /home/lmfdb/lmfdb-git-$branch
+    git fetch
+    git checkout origin/$branch -f
+    popd
+  fi
+end for
+
+# cmfs.lmfdb.xyz=scratch.lmfdb.xyz running on 8083 gunicorn-config-master
+# abvar.lmfdb.xyz running on 8084 gunicorn-config-abvar
+# blue.lmfdb.xyz running on 8090
+# olive.lmfdb.xyz running on 8091
+# pink.lmfdb.xyz running on 8092
+# purple.lmfdb.xyz running on 8093
+# red.lmfdb.xyz running on 8094
+# teal.lmfdb.xyz running on 8094
+# groups.lmfdb.xyz running on 8100
+# tori.lmfdb.xyz running on 8101
+for branch in master abvar blue olive pink purle red do
+  if [ -d "/home/lmfdb/lmfdb-git-$branch" ]; then
+    pushd /home/lmfdb/lmfdb-git-$branch
+    git fetch roed314 $branch
+    git checkout roed314/$branch -f
+    popd
+  fi
+end for
 
 
-# blue.lmfdb.xyz
-# running on 8090
-if [ -d "/home/lmfdb/lmfdb-git-blue" ]; then
-pushd /home/lmfdb/lmfdb-git-blue
-git fetch roed314 blue
-git checkout roed314/blue -f
-popd
-fi
 
-# olive.lmfdb.xyz
-# running on 8091
-if [ -d "/home/lmfdb/lmfdb-git-olive" ]; then
-pushd /home/lmfdb/lmfdb-git-olive
-git fetch roed314 olive
-git checkout roed314/olive -f
-popd
-fi
 
-# pink.lmfdb.xyz
-# running on 8092
-if [ -d "/home/lmfdb/lmfdb-git-pink" ]; then
-pushd /home/lmfdb/lmfdb-git-pink
-git fetch roed314 pink
-git checkout roed314/pink -f
-popd
-fi
 
-# purple.lmfdb.xyz
-# running on 8093
-if [ -d "/home/lmfdb/lmfdb-git-purple" ]; then
-pushd /home/lmfdb/lmfdb-git-purple
-git fetch roed314 purple
-git checkout roed314/purple -f
-popd
-fi
 
-# red.lmfdb.xyz
-# running on 8094
-if [ -d "/home/lmfdb/lmfdb-git-red" ]; then
-pushd /home/lmfdb/lmfdb-git-red
-git fetch roed314 red
-git checkout roed314/red -f
-popd
-fi
 
-# teal.lmfdb.xyz
-# running on 8094
-if [ -d "/home/lmfdb/lmfdb-git-teal" ]; then
-pushd /home/lmfdb/lmfdb-git-teal
-git fetch roed314 teal
-git checkout roed314/teal -f
-popd
-fi
+
+
 
 
 
