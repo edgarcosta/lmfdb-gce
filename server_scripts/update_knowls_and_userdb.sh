05b3797d9530ebeb686a24ed9ecadbea4599750a
Edgar Costa <edgarc@mit.edu>
2021-01-13 16:48:37 +0000

    everything is running postgres version 13

--- server_scripts/update_knowls_and_userdb.sh
+++ server_scripts/update_knowls_and_userdb.sh
@@ -12,8 +12,8 @@ else
   echo timestamp = `date -u +%Y%m%d-%H%M`
   userdbdump=/scratch/knowls_userdb/userdb`date -u +%Y%m%d-%H%M`.tar
   knowlsdump=/scratch/knowls_userdb/knowls`date -u +%Y%m%d-%H%M`.tar
-  time /usr/lib/postgresql/10/bin/pg_dump --host devmirror --clean --if-exists --schema=userdb -t 'userdb.users' -v --file $userdbdump --format tar lmfdb
-  time /usr/lib/postgresql/10/bin/pg_dump --host devmirror --clean --if-exists --schema=public -t 'kwl_(knowls|locks)'  -v --file $knowlsdump --format tar lmfdb
+  time pg_dump --host devmirror --clean --if-exists --schema=userdb -t 'userdb.users' -v --file $userdbdump --format tar lmfdb
+  time pg_dump --host devmirror --clean --if-exists --schema=public -t 'kwl_(knowls|locks)'  -v --file $knowlsdump --format tar lmfdb
   time pg_restore --single-transaction --clean --if-exists --dbname lmfdb $userdbdump
   time pg_restore --single-transaction --clean --if-exists --dbname lmfdb $knowlsdump
   du -sh $userdbdump $knowlsdump
