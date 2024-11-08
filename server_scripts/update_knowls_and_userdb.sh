3af2210bcb155d15fe1330c2856a0f49bb690cf8
Edgar Costa <edgarc@mit.edu>
2024-11-08 14:48:08 -0500

    adding --host /var/run/postgresql

--- server_scripts/update_knowls_and_userdb.sh
+++ server_scripts/update_knowls_and_userdb.sh
@@ -16,8 +16,8 @@ else
   time pg_dump --host devmirror --clean --if-exists --schema=userdb -t 'userdb.users' -v --file $userdbdump --format tar lmfdb
   time pg_dump --host devmirror --clean --if-exists --schema=public -t 'kwl_(knowls|locks)'  -v --file $knowlsdump --format tar lmfdb
   time psql --host devmirror --dbname lmfdb --command "\\copy (SELECT name, table_description, col_description FROM meta_tables) TO '$descdump' WITH CSV;"
-  time pg_restore --single-transaction --clean --if-exists --dbname lmfdb $userdbdump
-  time pg_restore --single-transaction --clean --if-exists --dbname lmfdb $knowlsdump
+  time pg_restore --host /var/run/postgresql --single-transaction --clean --if-exists --dbname lmfdb $userdbdump
+  time pg_restore --host /var/run/postgresql --single-transaction --clean --if-exists --dbname lmfdb $knowlsdump
   time psql --dbname lmfdb --command "BEGIN; CREATE TEMP TABLE tmp (name text,  table_description text, col_description jsonb); COPY tmp FROM '$descdump' (FORMAT csv); UPDATE meta_tables SET (table_description, col_description) = (tmp.table_description, tmp.col_description) FROM tmp WHERE meta_tables.name = tmp.name; DO language plpgsql \$\$ BEGIN RAISE NOTICE 'updating meta_tables done'; END \$\$; END;"
   du -sh $userdbdump $knowlsdump
   rm -rf $userdbdump $knowlsdump
